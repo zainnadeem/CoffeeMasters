340367a4adce2b3a7d628a3df728cc7585a438d0
Zain Nadeem <zn.nadeem@gmail.com>
2024-08-11 16:42:21 -0400

    first Commit

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -9,18 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Offer(title: "New Offer", description: "Hows it going")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewDevice("iPhone SE (3rd generation)")
+        }
     }
 }
